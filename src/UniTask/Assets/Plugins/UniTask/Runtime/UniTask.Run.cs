9c8067c9a2fe0aafe424cc0cded4387ab94e7a69
tkaria <tkaria.info@gmail.com>
2021-08-12 12:56:28 +0900

    Add 'RunOnMainThread'

--- src/UniTask/Assets/Plugins/UniTask/Runtime/UniTask.Run.cs
+++ src/UniTask/Assets/Plugins/UniTask/Runtime/UniTask.Run.cs
@@ -1,4 +1,4 @@
-﻿#pragma warning disable CS1591 // Missing XML comment for publicly visible type or member
+#pragma warning disable CS1591 // Missing XML comment for publicly visible type or member
 
 using System;
 using System.Threading;
@@ -442,6 +442,232 @@ namespace Cysharp.Threading.Tasks
             cancellationToken.ThrowIfCancellationRequested();
 
             await UniTask.SwitchToThreadPool();
+
+            cancellationToken.ThrowIfCancellationRequested();
+
+            if (configureAwait)
+            {
+                try
+                {
+                    return await func(state);
+                }
+                finally
+                {
+                    cancellationToken.ThrowIfCancellationRequested();
+                    await UniTask.Yield();
+                    cancellationToken.ThrowIfCancellationRequested();
+                }
+            }
+            else
+            {
+                var result = await func(state);
+                cancellationToken.ThrowIfCancellationRequested();
+                return result;
+            }
+        }
+
+      /// <summary>Run action on the main thread.</summary>
+        public static async UniTask RunOnMainThread(Action action, bool configureAwait = true, CancellationToken cancellationToken = default)
+        {
+            cancellationToken.ThrowIfCancellationRequested();
+
+            await UniTask.SwitchToMainThread();
+
+            cancellationToken.ThrowIfCancellationRequested();
+
+            if (configureAwait)
+            {
+                try
+                {
+                    action();
+                }
+                finally
+                {
+                    await UniTask.Yield();
+                }
+            }
+            else
+            {
+                action();
+            }
+
+            cancellationToken.ThrowIfCancellationRequested();
+        }
+
+        /// <summary>Run action on the main thread.</summary>
+        public static async UniTask RunOnMainThread(Action<object> action, object state, bool configureAwait = true, CancellationToken cancellationToken = default)
+        {
+            cancellationToken.ThrowIfCancellationRequested();
+
+            await UniTask.SwitchToMainThread();
+
+            cancellationToken.ThrowIfCancellationRequested();
+
+            if (configureAwait)
+            {
+                try
+                {
+                    action(state);
+                }
+                finally
+                {
+                    await UniTask.Yield();
+                }
+            }
+            else
+            {
+                action(state);
+            }
+
+            cancellationToken.ThrowIfCancellationRequested();
+        }
+
+        /// <summary>Run action on the main thread.</summary>
+        public static async UniTask RunOnMainThread(Func<UniTask> action, bool configureAwait = true, CancellationToken cancellationToken = default)
+        {
+            cancellationToken.ThrowIfCancellationRequested();
+
+            await UniTask.SwitchToMainThread();
+
+            cancellationToken.ThrowIfCancellationRequested();
+
+            if (configureAwait)
+            {
+                try
+                {
+                    await action();
+                }
+                finally
+                {
+                    await UniTask.Yield();
+                }
+            }
+            else
+            {
+                await action();
+            }
+
+            cancellationToken.ThrowIfCancellationRequested();
+        }
+
+        /// <summary>Run action on the main thread.</summary>
+        public static async UniTask RunOnMainThread(Func<object, UniTask> action, object state, bool configureAwait = true, CancellationToken cancellationToken = default)
+        {
+            cancellationToken.ThrowIfCancellationRequested();
+
+            await UniTask.SwitchToMainThread();
+
+            cancellationToken.ThrowIfCancellationRequested();
+
+            if (configureAwait)
+            {
+                try
+                {
+                    await action(state);
+                }
+                finally
+                {
+                    await UniTask.Yield();
+                }
+            }
+            else
+            {
+                await action(state);
+            }
+
+            cancellationToken.ThrowIfCancellationRequested();
+        }
+
+        /// <summary>Run action on the main thread.</summary>
+        public static async UniTask<T> RunOnMainThread<T>(Func<T> func, bool configureAwait = true, CancellationToken cancellationToken = default)
+        {
+            cancellationToken.ThrowIfCancellationRequested();
+
+            await UniTask.SwitchToMainThread();
+
+            cancellationToken.ThrowIfCancellationRequested();
+
+            if (configureAwait)
+            {
+                try
+                {
+                    return func();
+                }
+                finally
+                {
+                    await UniTask.Yield();
+                    cancellationToken.ThrowIfCancellationRequested();
+                }
+            }
+            else
+            {
+                return func();
+            }
+        }
+
+        /// <summary>Run action on the main thread.</summary>
+        public static async UniTask<T> RunOnMainThread<T>(Func<UniTask<T>> func, bool configureAwait = true, CancellationToken cancellationToken = default)
+        {
+            cancellationToken.ThrowIfCancellationRequested();
+
+            await UniTask.SwitchToMainThread();
+
+            cancellationToken.ThrowIfCancellationRequested();
+
+            if (configureAwait)
+            {
+                try
+                {
+                    return await func();
+                }
+                finally
+                {
+                    cancellationToken.ThrowIfCancellationRequested();
+                    await UniTask.Yield();
+                    cancellationToken.ThrowIfCancellationRequested();
+                }
+            }
+            else
+            {
+                var result = await func();
+                cancellationToken.ThrowIfCancellationRequested();
+                return result;
+            }
+        }
+
+        /// <summary>Run action on the main thread.</summary>
+        public static async UniTask<T> RunOnMainThread<T>(Func<object, T> func, object state, bool configureAwait = true, CancellationToken cancellationToken = default)
+        {
+            cancellationToken.ThrowIfCancellationRequested();
+
+            await UniTask.SwitchToMainThread();
+
+            cancellationToken.ThrowIfCancellationRequested();
+
+            if (configureAwait)
+            {
+                try
+                {
+                    return func(state);
+                }
+                finally
+                {
+                    await UniTask.Yield();
+                    cancellationToken.ThrowIfCancellationRequested();
+                }
+            }
+            else
+            {
+                return func(state);
+            }
+        }
+
+        /// <summary>Run action on the main thread.</summary>
+        public static async UniTask<T> RunOnMainThread<T>(Func<object, UniTask<T>> func, object state, bool configureAwait = true, CancellationToken cancellationToken = default)
+        {
+            cancellationToken.ThrowIfCancellationRequested();
+
+            await UniTask.SwitchToMainThread();
 
             cancellationToken.ThrowIfCancellationRequested();
 
